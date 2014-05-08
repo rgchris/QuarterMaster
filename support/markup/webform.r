709b872b7a1c8e088ccc020cb066d020d8dccbed
Christopher Ross-Gill <chris.rg@ross-gill.com>
2014-05-08 02:26:16 -0400

    SUPPORT: AltWebForm to v0.1.6

--- support/markup/webform.r
+++ support/markup/webform.r
@@ -1,32 +1,38 @@
-REBOL [
-	Title: "REBOL <-> Web Form"
+Rebol [
+	Title: "REBOL Web Form Encoder/Decoder"
 	Author: "Christopher Ross-Gill"
-	Date: 13-Jul-2011
-	Version: 0.1.5
+	Date: 30-Jan-2013
+	Version: 0.1.6
 	Purpose: "Convert a Rebol block to a URL-Encoded Web Form string"
 	Comment: "Conforms to application/x-www-form-urlencoded"
+	Home: http://www.ross-gill.com/page/Web_Forms_and_REBOL
 	File: %altwebform.r
 	Type: 'module
-	Exports: [url-decode load-webform url-encode to-webform]
+	Exports: [url-decode url-encode load-webform to-webform]
 	Example: [
 		"a=3&aa.a=1&b.c=1&b.c=2"
 		[a "3" aa [a "1"] b [c ["1" "2"]]]
 	]
 ]
 
-url-decode: use [deplus decrlf][
+url-decode: use [deplus sp decrlf][
 	deplus: func [text][
-		parse/all text [any [to #"+" text: (change text #" ")] to end]
+		parse/all text [
+			some [to sp text: (text: change text #" ") :text] to end
+		]
 		head text
 	]
 
 	decrlf: func [text][
-		parse/all text [any [to crlf text: (remove text)] to end]
+		parse/all text [
+			some [to crlf text: (text: change/part text #"^/" 2) :text] to end
+		]
 		head text
 	]
 
-	func [text [any-string! none!]][
-		decrlf dehex deplus either text [to-string text][""]
+	func [text [any-string! none!] /wiki][
+		sp: either wiki [#"_"][#"+"]
+		decrlf dehex deplus form any [text ""]
 	]
 ]
 
@@ -36,15 +42,15 @@ load-webform: use [result path string pair as-path][
 	as-path: func [name [string!]][to-path to-block replace/all name #"." #" "]
 
 	path: use [aa an wd][
-		aa: #[bitset! 64#{AAAAAAAAAAD+//8H/v//BwAAAAAAAAAAAAAAAAAAAAA=}]
-		an: #[bitset! 64#{AAAAAAAg/wP+//+H/v//BwAAAAAAAAAAAAAAAAAAAAA=}]
-		wd: [aa 0 40 an] ; one alpha, any alpha/numeric/dash/underscore
+		aa: charset [#"A" - #"Z" #"a" - #"z"]
+		an: union aa charset ["-_" #"0" - #"9"]
+		wd: [aa 0 40 an] ; one alpha, any alpha/digit/dash/underscore
 		[wd 0 6 [#"." wd]]
 	]
 
 	string: use [ch hx][
-		ch: charset {*-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz!$'(),/:;?@[\]^^`{|}~}
-		hx: #[bitset! 64#{AAAAAAAA/wN+AAAAfgAAAAAAAAAAAAAAAAAAAAAAAAA=}]
+		ch: charset ["!'*,-._~" #"0" - #"9" #"A" - #"Z" #"a" - #"z"]
+		hx: charset [#"0" - #"9" #"A" - #"F" #"a" - #"f"]
 		[any [ch | #"+" | #"%" 2 hx]] ; any [unreserved | percent-encoded]
 	]
 
@@ -89,21 +95,26 @@ load-webform: use [result path string pair as-path][
 	]
 ]
 
-url-encode: use [ch][
-	ch: #[bitset! 64#{AAAAAABg/wP+//+H/v//RwAAAAAAAAAAAAAAAAAAAAA=}]
-	func [text [any-string!]][
-		either parse/all copy text [
-			any [
-				some ch | #" " text: (change back text #"+") |
-				skip text: (change/part back text join "%" enbase/base to-string text/-1 16 1)
+url-encode: use [ch sp encode][
+	ch: charset ["!'*,-.~" #"0" - #"9" #"A" - #"Z" #"a" - #"z"]
+	encode: func [text][insert next text enbase/base form text/1 16 change text "%"]
+
+	func [text [any-string!] /wiki][
+		sp: either wiki [#"_"][#"+"]
+
+		parse/all copy text [
+			copy text any [
+				  text: some ch | #" " (change text sp)
+				| #"_" (all [wiki encode text]) | skip (encode text)
 			]
-		][head text][""]
+		]
+		text
 	]
 ]
 
 to-webform: use [
 	webform form-key emit
-	here path value block array object
+	here path reference value block array object
 ][
 	path: []
 	form-key: does [
@@ -112,6 +123,10 @@ to-webform: use [
 
 	emit: func [data][
 		repend webform ["&" form-key "=" url-encode data]
+	]
+
+	reference: [
+		here: get-word! (change/only here attempt [get/any here/1]) :here
 	]
 
 	value: [
@@ -127,7 +142,7 @@ to-webform: use [
 	object: [
 		any [
 			here: [word! | set-word!] (insert path to-word here/1)
-			[value | block] (remove path)
+			any reference [value | block] (remove path)
 		] end
 	]
 
@@ -135,7 +150,7 @@ to-webform: use [
 		here: [
 			  any-block! (change/only here copy here/1)
 			| object! (change/only here third here/1)
-		] :here into [object | array]
+		] :here into [object | mk: array]
 	]
 
 	func [
